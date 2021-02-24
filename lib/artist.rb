ab6ae54cb7d6fc7d286f0d1bd58e64827cb4bb89
github username <github email address>
2021-02-24 10:37:45 -0500

    commit tree 314907f06e2705e3965e1444b477bcced94d3d8e

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+class Artist
+    attr_accessor :name
+
+    def initialize
+        @name = name
+    end
+
+end
+
+#does the name not get initialied? does it get passed when creating a different obj 
+#in a different class?
